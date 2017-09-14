c7ff3516ff858ff420f4a1a3980f830642fea696
socket1016 <socket1016@users.noreply.github.com>
2017-09-14 21:32:11 +0900

    [amazon-buy-gift]待ち時間を少し縮める、確認画面は1.5秒以下にするとロード終わらないことが多い

--- amazon-auto-buy-gift.js
+++ amazon-auto-buy-gift.js
@@ -12,20 +12,20 @@
     const href = location.href;
     // 引数の秒数分待つ
     const wait = ( /** @type {number} */ second) => new Promise((resolve) => setTimeout(() => resolve(), second * 1000))
-    await wait(1)
+    await wait(0.7)
     // 3000円入力画面
     if (href.includes("https://www.amazon.co.jp/gp/gc/create")) {
         const input = /** @type {HTMLInputElement} */ (document.querySelector("#gc-asv-manual-reload-amount"));
         const button = /** @type {HTMLElement} */ (document.querySelector("#form-submit-button"));
         input.value = "3000";
-        await wait(0.5);
+        await wait(0.3);
         button.click();
     // お支払方法選択画面
     } else if (href.includes("https://www.amazon.co.jp/gp/buy/payselect/handlers/display.html?hasWorkingJavascript=1")) {
         const checkbox = /** @type {HTMLElement} */ (document.querySelector("#convenienceStorePayment"));
         const continueButton = /** @type {HTMLElement} */ (document.querySelector("#continue-top"));
         checkbox.click();
-        await wait(0.5);
+        await wait(0.3);
         continueButton.click();
         // 確認画面
         await wait(2);
